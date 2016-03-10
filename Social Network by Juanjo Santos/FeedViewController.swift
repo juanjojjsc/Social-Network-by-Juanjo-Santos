08b5deb769b1789e994507cf853b773d414d769b
Juanjo Santos <juanjo@digitalcoaster.mx>
2016-03-10 13:16:43 -0600

    VERSION ESTABLE SIN POSTS

--- Social Network by Juanjo Santos/FeedViewController.swift
+++ Social Network by Juanjo Santos/FeedViewController.swift
@@ -15,6 +15,9 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet weak var tableView: UITableView!
     var posts = [Post]()
     
+    //CREAR EL CACHE STATIC: ES QUE ES UNICO Y GLOBALLY ACCESIBLE
+    static var imageCache = NSCache()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,13 +26,16 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         tableView.delegate = self
         tableView.dataSource = self
         
+        tableView.estimatedRowHeight = 384
+        
         //THIS IS HOW WE DOWNLOAD DATA FROM FIREBASE SE HACE EN EL VIEW DID LOAD
         
         // called whenever a value is updated on Firebase
         // first we get what we already have and then listen for changes
         DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: { snapshot in
             //print the changed data
-            print(snapshot.value)
+            //print(snapshot.value)
+            
             
             
             //VACIAR EL ARRAY
@@ -92,20 +98,45 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         ------------------------------------------------------------------------------------
         ------------------------------------------------------------------------------------
         ------------------------------------------------------------------------------------
-        
-        
-        
         */
         
-        let currentPost = posts[indexPath.row]
+        
         
         if let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as? StylishPostTableViewCell {
             
-            cell.configureCell(currentPost)
+            //Cancelamos la request cada vez que scrolleamos en la table view
+            // para que no se descargue la anterior
+            cell.request?.cancel()
+            
+            let currentPost = posts[indexPath.row]
+            
+            var cachedImg: UIImage?
+            
+            if let url = currentPost.imgUrl {
+                //GET THE IMAGE FROM THE CACHE with its own KET that is the URL
+                //if it exists
+                cachedImg = FeedViewController.imageCache.objectForKey(url) as? UIImage
+            }
+            
+            // if the image was assigned,pass it if it wasnt pass the empty image
+            cell.configureCell(currentPost, recievedImg: cachedImg)
             return cell
             
         } else {
             return StylishPostTableViewCell()
+        }
+        
+    }
+    
+    //SI NO HAY IMAGEN, HACER LA CELL MAS CHICA
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        let currentPost = posts[indexPath.row]
+        
+        if currentPost.imgUrl == nil {
+            return 130
+        } else {
+            return tableView.estimatedRowHeight
         }
         
     }
--- Social Network by Juanjo Santos/StylishPostTableViewCell.swift
+++ Social Network by Juanjo Santos/StylishPostTableViewCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class StylishPostTableViewCell: UITableViewCell {
 
@@ -17,6 +18,8 @@ class StylishPostTableViewCell: UITableViewCell {
     @IBOutlet weak var likesLbl: UILabel!
     
     var post: Post!
+    // request de tipo Firebase. La guardamos para poder cancelarla
+    var request: Request?
     
     
     override func awakeFromNib() {
@@ -41,7 +44,7 @@ class StylishPostTableViewCell: UITableViewCell {
     
     // SUPERIMPORTANTE QUE HAGAMOS LA CONFIGURACION DE LA CELDA AQUI!!!! Y NO EN CELLFORROWATINDEXPATH
     
-    func configureCell(recievedPost: Post) {
+    func configureCell(recievedPost: Post, recievedImg: UIImage?) {
         
         self.post = recievedPost
         
@@ -50,7 +53,51 @@ class StylishPostTableViewCell: UITableViewCell {
         self.likesLbl.text = "\(post.likes)"
         
         
+        // ---------------------------------------------------------------------------------
+        // ---------------------------------------------------------------------------------
+        // ---------------------------------------------------------------------------------
+        // ---------------------------------------------------------------------------------
+        // ---------------------------------------------------------------------------------
+        //                      DESCARGAMOS LAS IMAGENES AL CACHE QUE ES VOLATIL
+        //               ASI SOLO LAS DESCARGAMOS UNA VEZ CADA QUE SE REFRESCA LA TABLA
+        // ---------------------------------------------------------------------------------
+        // ---------------------------------------------------------------------------------
+        // ---------------------------------------------------------------------------------
+        // ---------------------------------------------------------------------------------
+        // ---------------------------------------------------------------------------------
         
+        
+        //Si si recivimos una imagen, poruqe podria ser que no puso imagen el usuario
+        if recievedPost.imgUrl != nil {
+            //si si hay imagen tenemos que descargarla
+            
+            //SI RECIBIMOS ALGO, ES PORQUE YA ESTA GUARDADA EN EL CACHE
+            if recievedImg != nil {
+                
+                //LOADEARLA DESDE EL CACHE
+                self.postImage.image = recievedImg
+            } else {
+                
+                //Si no recibimos algo, si fue nil, NO ESTA EN EL CACHE, ENTONCES DESCARGARLA
+                request = Alamofire.request(.GET, recievedPost.imgUrl!).validate(contentType: ["image/*"]).response(completionHandler: { request, response, data, err in
+                    
+                    if err == nil {
+                        let downloadedImg = UIImage(data: data!)!
+                        self.postImage.image = downloadedImg
+                        //ya que esta descargada
+                        
+                        //GUARDARLA EN EL CACHE
+                        FeedViewController.imageCache.setObject(downloadedImg, forKey: self.post.imgUrl!)
+                    }
+                    
+                }) //make sure we downloaded an image
+            }
+            
+        
+        } else {
+            //Si no hubo imagen,entonces escondemos el ImageView
+            self.postImage.hidden = true
+        }
         
         
     }
